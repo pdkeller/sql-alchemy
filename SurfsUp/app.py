d3bb0b09fcd9b17dbb9413c8cb9818944be2e383
Paul Keller <paulkeller99@att.net>
2024-09-30 21:35:05 -0500

    Main commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,75 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap()
 
 # reflect the tables
-
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home:
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+        )
+
+
+@app.route("/api/v1.0/precipitation")
+
+
+
+@app.route("/api/v1.0/stations")
+station_query = session.query(Station.station).all()
+
+return jsonify(station_query)
+
+
+@app.route("/api/v1.0/tobs")
+
+
+
+@app.route("/api/v1.0/<start>")
+
+
+
+@app.route("/api/v1.0/<start>/<end>")
+session.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
